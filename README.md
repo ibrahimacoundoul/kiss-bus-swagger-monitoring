34b984cb521be6d324f6e264c6100ce0fd97bb1e
Ibrahima COUNDOUL <Ibrahima.COUNDOUL@cpexterne.org>
2014-12-16 14:21:44 +0100

    commit auto des modifications des webservices KISS avec Swagger (shell)

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-c'est cool github!!! 
+Gestion de la documentation des API des Webservices de KISS pour le BUS.
--- kiss-swagger-monitor.sh
+++ kiss-swagger-monitor.sh
@@ -2,17 +2,21 @@
 #icoundoul,10/12/2014
 
 
-#cd /var/lib/jenkins/jobs/kiss-bus-swagger-monitoring
 
+#Jump to project customer directeory
 cd /soft/kiss-bus-swagger-monitoring 
 
+#Setting the rigth permissions to the json files
 chmod 777 -R src/main/resources/*.json
 
+#Getting the jar file for the main class that generates the json files
 cp /var/lib/jenkins/jobs/kiss-bus-swagger-monitoring-build/workspace/target/kiss-swagger-monitoring-dep.jar .
 
+#Upding the current repos
 git pull origin master
 
-java -cp kiss-swagger-monitoring-dep.jar fr.canalplus.cgaweb.swagger.kissSwaggerDiff.KissWSApiDocsMonitoring # regeneration des fichiers json
+# generat the files 
+java -cp kiss-swagger-monitoring-dep.jar fr.canalplus.cgaweb.swagger.kissSwaggerDiff.KissWSApiDocsMonitoring
 
 
 #git pull origin master
